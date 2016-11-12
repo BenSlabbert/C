b7bfcfe871de850cfb7319b7f96c9b6c86206346
Benjamin Slabbert <benjamin@impactradius.com>
2016-11-12 22:03:02 +0200

    adding project

---
+++ testApplication/main.c
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: benjamin
+ *
+ * Created on 12 November 2016, 21:42
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "myHeader.h"
+
+/*
+ * main function
+ */
+int main(int argc, char** argv) {
+    
+    printf("got 5? : %d", get5());
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ testApplication/myHeader.h
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   myHeader.h
+ * Author: benjamin
+ *
+ * Created on 12 November 2016, 21:51
+ */
+
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    int get5(){
+        return 5;
+    }
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MYHEADER_H */
+
